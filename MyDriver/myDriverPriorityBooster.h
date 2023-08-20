621e1f061e295cd82b4bb4333e7aeb7f2643f289
Nilesh-cvr <110968327+Nilesh-cvr@users.noreply.github.com>
2023-08-20 18:02:11 +0530

    Update myDriverPriorityBooster.h

--- MyDriver/myDriverPriorityBooster.h
+++ MyDriver/myDriverPriorityBooster.h
@@ -2,14 +2,15 @@
 #define _MYDRIVERPRIORITY_BOOSTER_H_
 
 
-
+#define DEVICE_NAME L"\\Device\\PrioBoss"   
+#define DRIVER_DOS_NAME L"\\??\\PrioBoss"   
+#define DRIVER_WIN_NAME L"\\\\.\\PrioBoss"
 
 #define PRIORITY_BOOSTER_DEVICE 0x8000
 
 #define IOCTL_PRIORITY_BOOSTER_SET_PRIORITY  CTL_CODE(PRIORITY_BOOSTER_DEVICE, 0x800, \
 		METHOD_NEITHER,FILE_ANY_ACCESS)
 
-//typedef unsigned long ULONG;
 
 struct ThreadData {
 	ULONG ThreadId;
